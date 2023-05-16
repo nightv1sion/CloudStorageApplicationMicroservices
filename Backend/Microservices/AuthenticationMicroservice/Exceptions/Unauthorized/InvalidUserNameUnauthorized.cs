4bc393b7ffa5d3c9f6917619103acfc5b3b32123
Danila Uprivanov <duprivanov@pmsoft.ru>
2023-05-16 21:38:25 +0300

    Logging points in the code

---
+++ Backend/Microservices/AuthenticationMicroservice/Exceptions/Unauthorized/InvalidUserNameUnauthorized.cs
@@ -0,0 +1,12 @@
+﻿using Middlewares.ExceptionHandling.Exceptions;
+
+namespace AuthenticationMicroservice.Exceptions.Unauthorized;
+
+public class InvalidUserNameUnauthorized : UnauthorizedException
+{
+    public InvalidUserNameUnauthorized(string username) : base(
+        $"User with username '{username}' does not exist")
+    {
+
+    }
+}
--- Backend/Shared/Middlewares/ExceptionHandling/ExceptionHandlingMiddleware.cs
+++ Backend/Shared/Middlewares/ExceptionHandling/ExceptionHandlingMiddleware.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using Middlewares.ExceptionHandling.Exceptions;
 
 namespace Middlewares.ExceptionHandling;
@@ -7,14 +8,19 @@ namespace Middlewares.ExceptionHandling;
 public class ExceptionHandlingMiddleware
 {
     private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
 
-    public ExceptionHandlingMiddleware(RequestDelegate next)
+    public ExceptionHandlingMiddleware(
+        RequestDelegate next, 
+        ILogger<ExceptionHandlingMiddleware> logger)
     {
         _next = next;
+        _logger = logger;
     }
 
     public async Task Invoke(HttpContext context)
     {
+        Exception ex = null;
         try
         {
             await _next(context);
@@ -24,10 +30,22 @@ public class ExceptionHandlingMiddleware
             context.Response.ContentType = "application/json";
             context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
             await context.Response.WriteAsync(exception.Message);
+            ex = exception;
         }
-        catch (UnauthorizedException)
+        catch (UnauthorizedException exception)
         {
             context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+            ex = exception;
+        }
+        catch (Exception exception)
+        {
+            ex = exception;
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        }
+
+        if (ex is not null)
+        {
+            _logger.LogError(ex.ToString());
         }
     }
 }
