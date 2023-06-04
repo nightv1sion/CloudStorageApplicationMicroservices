232271cdbace03ee80c6e0df714179dd593ab0e0
Danila Uprivanov <duprivanov@pmsoft.ru>
2023-06-04 22:11:30 +0300

    Getting and updating files records

---
+++ Backend/Microservices/FileStorage/FileStorage.API/DataTransferObjects/UpdateFileDto.cs
@@ -0,0 +1,7 @@
+﻿namespace FileStorage.API.DataTransferObjects;
+
+public class UpdateFileDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
--- Backend/Microservices/FileStorage/FileStorage.API/Services.Contracts/IFileService.cs
+++ Backend/Microservices/FileStorage/FileStorage.API/Services.Contracts/IFileService.cs
@@ -1,10 +1,14 @@
 ﻿using FileStorage.API.Model;
 using FileStorage.API.DataTransferObjects;
+using File = FileStorage.API.Model.File;
 
 namespace FileStorage.API.Services.Contracts;
 
 public interface IFileService
 {
-    public Task<(byte[] file, string fileName)> GetFileBytesAsync(Guid fileId);
-    public Task SaveFileToStorageAsync(FileDto dto, Guid userId);
+    public Task<File> GetFileAsync(Guid userId, Guid fileId);
+    public Task<ICollection<File>> GetFilesByUserIdAsync(Guid userId);
+    public Task UpdateFileAsync(Guid userId, UpdateFileDto dto);
+    public Task<(byte[] file, string fileName)> GetFileBytesAsync(Guid userId, Guid fileId);
+    public Task SaveFileToStorageAsync(FormFileDto dto, Guid userId);
 }
