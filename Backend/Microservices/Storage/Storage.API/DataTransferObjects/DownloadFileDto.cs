8ab8f4191322eb968fff22203faf6f139efecd9b
Danila Uprivanov <mr.uprivanov@yandex.ru>
2023-09-21 23:17:34 +0300

    remove Backend/Microservices/Storages

--- Backend/Microservices/Storage/Storage.API/DataTransferObjects/DownloadFileDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Storage.API.DataTransferObjects;
-
-public class DownloadFileDto
-{
-    public string StorageFileName { get; set; }
-    public string OriginalName { get; set; }
-}
--- Backend/Microservices/Storage/Storage.API/Exceptions/InvalidFileNameBadRequestException.cs
+++
@@ -1,8 +0,0 @@
-﻿using Middlewares.ExceptionHandling.Exceptions;
-
-namespace Storage.API.Exceptions;
-
-public class InvalidFileNameBadRequestException : BadRequestException
-{
-    public InvalidFileNameBadRequestException(string fileName) : base($"File with name '{fileName}' does not exist") {}
-}
