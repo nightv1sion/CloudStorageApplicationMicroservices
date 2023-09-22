c29ae65de927a6e263771fec3cf26b5a309ece36
Danila Uprivanov <mr.uprivanov@yandex.ru>
2023-09-23 00:48:43 +0300

    remove Backend/Shared/Routes

--- Backend/Shared/Routes/Helpers/IRoutePatternHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Routing;
-
-namespace Routes.Helpers;
-
-public interface IRoutePatternHelper
-{
-    RouteValueDictionary GetRouteValues(string routePattern, string route);
-    string SetRouteValuesIntoPattern(string routePattern, RouteValueDictionary routeValues);
-}
