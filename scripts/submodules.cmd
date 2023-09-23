e9121924e064d2b042b1192e3c55cf61716ed258
Danila Uprivanov <mr.uprivanov@yandex.ru>
2023-09-23 22:38:23 +0300

    remove scripts

--- scripts/submodules.cmd
+++
@@ -1,7 +0,0 @@
-git submodule add https://github.com/nightv1sion/night-cloud-backend-shared-routes backend/common/routes
-
-git submodule add https://github.com/nightv1sion/night-cloud-backend-shared-queue-messaging backend/common/queue-messaging
-
-git submodule add https://github.com/nightv1sion/night-cloud-backend-shared-middlewares backend/common/middlewares
-
-git submodule add https://github.com/nightv1sion/night-cloud-backend-shared-database-infrastructure backend/common/database-infrastructure
