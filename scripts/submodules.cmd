050e613268f87977b83fd29842c02a1293001a95
Danila Uprivanov <mr.uprivanov@yandex.ru>
2023-09-23 22:32:26 +0300

    add common submodules

---
+++ scripts/submodules.cmd
@@ -0,0 +1,7 @@
+git submodule add https://github.com/nightv1sion/night-cloud-backend-shared-routes backend/common/routes
+
+git submodule add https://github.com/nightv1sion/night-cloud-backend-shared-queue-messaging backend/common/queue-messaging
+
+git submodule add https://github.com/nightv1sion/night-cloud-backend-shared-middlewares backend/common/middlewares
+
+git submodule add https://github.com/nightv1sion/night-cloud-backend-shared-database-infrastructure backend/common/database-infrastructure
